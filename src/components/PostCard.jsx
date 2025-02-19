1965282574c78d292b7b99f3debfc087edf018bd
arunacharya1603 <arunacharya1603@gmail.com>
2025-02-19 21:56:58 +0530

    update: image fixed

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import appwriteservice from "../appwrite/config.js"
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import PropTypes from 'prop-types'
 
 
 function PostCard({
@@ -12,7 +10,7 @@ function PostCard({
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 p-4 rounded-xl'>
         <div className='w-full justify-center mb-4'>
-            <LazyLoadImage src={appwriteservice.getFilePreview(featuredImage)} alt={title} className='w-full h-48 object-cover rounded-xl' />
+            <img src={appwriteservice.getFilePreview(featuredImage)} alt={title} className='w-full h-48 object-cover rounded-xl' />
         </div>
         <h2 className='text-xl font-bold'>
             {title}
@@ -20,6 +18,12 @@ function PostCard({
         </div>
     </Link>
   )
+}
+
+PostCard.propTypes = {
+    $id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    featuredImage: PropTypes.string.isRequired
 }
 
 export default PostCard
