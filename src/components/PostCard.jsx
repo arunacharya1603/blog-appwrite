bed376a188d5062c9df147415f804da4ec744a9a
arunacharya1603 <arunacharya1603@gmail.com>
2025-02-19 22:01:57 +0530

    update: fixed image issue after image compression

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom'
 import appwriteservice from "../appwrite/config.js"
 import PropTypes from 'prop-types'
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function PostCard({
     $id, title, featuredImage
 }) {
+  const filePreview = featuredImage ? appwriteservice.getFilePreview(featuredImage) : null;
+  
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 p-4 rounded-xl'>
         <div className='w-full justify-center mb-4'>
-            <img src={appwriteservice.getFilePreview(featuredImage)} alt={title} className='w-full h-48 object-cover rounded-xl' />
+            {filePreview && (
+              <LazyLoadImage 
+                src={filePreview} 
+                alt={title} 
+                className='w-full h-48 object-cover rounded-xl'
+              />
+            )}
         </div>
         <h2 className='text-xl font-bold'>
             {title}
@@ -23,7 +32,7 @@ function PostCard({
 PostCard.propTypes = {
     $id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    featuredImage: PropTypes.string.isRequired
+    featuredImage: PropTypes.string
 }
 
 export default PostCard
